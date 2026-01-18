c7ac37d99ffe00fc8ed3ea6876d510ba2ec25ee4
anushkaams <anushkaams@gmail.com>
2026-01-18 18:59:57 +1100

    Add google metadata verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,17 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import Script from "next/script";
 
+export const metadata = {
+  title: "WebTools",
+  description: "Your all-in-one online tools hub",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  verification: {
+    google: "A9JzXDaah5gKJwBURGbXZlZ5ufO5ZFQ8qLnZS8a0Z4K8", // optional Google verification
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -10,24 +21,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <title>WebTools</title>
-        <link rel="icon" href="/favicon.ico" /> {/* Your exported logo here */}
-        <meta name="google-adsense-account" content="ca-pub-8839810575741990" />
+      <body className="min-h-screen flex flex-col">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Main content */}
+        <main className="flex-1 pt-24">{children}</main>
+
+        {/* Footer */}
+        <Footer />
+
+        {/* Google Adsense */}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8839810575741990"
           crossOrigin="anonymous"
           strategy="afterInteractive"
-        ></Script>
-      </head>
-      <body className="min-h-screen flex flex-col">
-        <Navbar />
-
-        {/* Main grows to push footer down */}
-        <main className="flex-1 pt-24">{children}</main>
-
-        <Footer />
+        />
       </body>
     </html>
   );
